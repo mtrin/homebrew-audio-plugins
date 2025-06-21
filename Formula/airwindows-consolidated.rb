156211def69c8802aa6327d69f60cdb3eb1df42f
mtrin <mauriciotrindade+github@gmail.com>
2025-06-22 01:37:58 +1000

    Add source_type attribute to airwindows-consolidated formula

--- Formula/airwindows-consolidated.rb
+++ Formula/airwindows-consolidated.rb
@@ -3,6 +3,7 @@ class AirwindowsConsolidated < Formula
   desc "Airwindows Consolidated macOS DAW Plugin"
   homepage "https://github.com/baconpaul/airwin2rack"
   url "https://github.com/baconpaul/airwin2rack/releases/download/DAWPlugin/airwindows-consolidated-macOS-2025-06-21-5ea9f35.dmg"
+  source_type :github
   sha256 "0fbae22d40b590f71fcc50e5c48379ad3b5ea01c8b73669c8f4dab66f4f83c64"
 
   livecheck do
