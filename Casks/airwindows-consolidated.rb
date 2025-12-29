58e65a30f8518e5ba5ffbe2cb738486722f3bcb6
mtrin <mauriciotrindade+github@gmail.com>
2025-12-29 22:38:23 +1100

    update cask

--- Casks/airwindows-consolidated.rb
+++ Casks/airwindows-consolidated.rb
@@ -4,7 +4,7 @@ cask "airwindows-consolidated" do
 
   url "https://github.com/baconpaul/airwin2rack/releases/download/DAWPlugin/airwindows-consolidated-macOS-#{version}.dmg"
   name "Airwindows Consolidated"
-  desc "Airwindows Consolidated DAW Plugin"
+  desc "Consolidated DAW plugin for Chris's Airwindows effects"
   homepage "https://github.com/baconpaul/airwin2rack"
 
   livecheck do
@@ -13,17 +13,23 @@ cask "airwindows-consolidated" do
     strategy :github_releases do |json, regex|
       json.filter_map do |release|
         next if release["draft"] || release["prerelease"]
+
         release["assets"]&.filter_map { |a| a["name"]&.[](regex, 1) }
       end.flatten
     end
   end
 
   preflight do
-    system_command "/usr/bin/xar", args: ["-xf", "#{staged_path}/airwindows-consolidated-macOS-#{version}.pkg", "-C", staged_path]
-    system_command "/usr/bin/tar", args: ["-xf", "#{staged_path}/airwindows-consolidated_VST3.pkg/Payload", "-C", staged_path]
+    system_command "/usr/bin/xar",
+                   args: ["-xf", "#{staged_path}/airwindows-consolidated-macOS-#{version}.pkg",
+                          "-C", staged_path]
+    system_command "/usr/bin/tar",
+                   args: ["-xf", "#{staged_path}/airwindows-consolidated_VST3.pkg/Payload",
+                          "-C", staged_path]
   end
 
-  vst3_plugin "Airwindows Consolidated.vst3", target: "/Library/Audio/Plug-Ins/VST3/Airwindows Consolidated.vst3"
+  vst3_plugin "Airwindows Consolidated.vst3",
+              target: "/Library/Audio/Plug-Ins/VST3/Airwindows Consolidated.vst3"
 
   uninstall delete: "/Library/Audio/Plug-Ins/VST3/Airwindows Consolidated.vst3"
 end
